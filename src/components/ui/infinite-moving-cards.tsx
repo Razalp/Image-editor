40a10ad3c6001357c2943b3c04bb71ffc16275ba
Razalp <razalp0012300@gmail.com>
2024-04-25 11:59:35 +0530

    redploy

--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -89,7 +89,7 @@ export const InfiniteMovingCards = ({
       >
 
 
-        {items.map((item, idx) => (
+        {items.map((item) => (
          <li
          className="w-[350px] max-w-full relative rounded-2xl border border-b-0 flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
          style={{
